deb25bbcc06e60bb4156b981a887222d713c4e11
Luke Pighetti <lukepighetti@gmail.com>
2022-02-03 13:52:23 -0500

    add styles

--- packages/styles/lib/styles.dart
+++ packages/styles/lib/styles.dart
@@ -1,7 +1,22 @@
 library styles;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static final primary = Colors.blue.shade500;
+  static final secondary = Colors.amberAccent.shade400;
+  static final error = Colors.red.shade400;
+
+  static const white = Colors.white;
+  static const black = Colors.black;
+
+  static final grey1 = Colors.grey.shade100;
+  static final grey2 = Colors.grey.shade200;
+  static final grey3 = Colors.grey.shade300;
+  static final grey4 = Colors.grey.shade400;
+  static final grey5 = Colors.grey.shade500;
+  static final grey6 = Colors.grey.shade600;
+  static final grey7 = Colors.grey.shade700;
+  static final grey8 = Colors.grey.shade800;
+  static final grey9 = Colors.grey.shade900;
 }
