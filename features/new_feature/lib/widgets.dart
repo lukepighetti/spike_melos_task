5f501189bccbd1a5b08a1da6c6fd49eac495e34b
Luke Pighetti <lukepighetti@gmail.com>
2022-02-07 18:42:21 -0500

    add bricks/library and bricks/feature

---
+++ features/new_feature/lib/widgets.dart
@@ -0,0 +1,12 @@
+library widgets;
+
+import 'package:flutter/material.dart';
+
+class HelloWidget extends StatelessWidget {
+  const HelloWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello!');
+  }
+}
